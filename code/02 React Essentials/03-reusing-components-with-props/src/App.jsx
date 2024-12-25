27c556ea35221a7c3eee88bed523ade451c4252c
Benjamin Heimann <benjamin.heimann@outlook.de>
2024-12-25 21:20:06 +0100

    Add 03-reusing-components-with-props

--- code/02 React Essentials/03-reusing-components-with-props/src/App.jsx
+++ code/02 React Essentials/03-reusing-components-with-props/src/App.jsx
@@ -1,4 +1,5 @@
 import reactImage from './assets/react-core-concepts.png';
+import { CORE_CONCEPTS } from './data';
 
 const descriptions = ['Fundamental', 'Crucial', 'Core'];
 
@@ -25,8 +26,27 @@ function Header() {
 function Main() {
   return (
     <main>
-      <h2>Time to get started!</h2>
+      <section id="core-concepts">
+        <h2>Core Concepts</h2>
+        <ul>
+          {CORE_CONCEPTS.map((concept) => (
+            <Tile title={concept.title} description={concept.description} image={concept.image} />
+            // <Tile {...concept} /> - does the same thing
+          ))}
+        </ul>
+      </section>
     </main>
+  );
+}
+
+// React components accept a single object parameter (props). Its properties can be extracted using object destructuring
+function Tile({ image, title, description }) {
+  return (
+    <li>
+      <img src={image} alt={title} />
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </li>
   );
 }
 
