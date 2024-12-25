334a8e3024a3e046c718f73b6109e103ca3efcf1
Benjamin Heimann <benjamin.heimann@outlook.de>
2024-12-25 21:34:24 +0100

    Add 04-store-components-in-different-files

---
+++ code/02 React Essentials/04-store-components-in-different-files/src/components/Main.jsx
@@ -0,0 +1,18 @@
+import { CORE_CONCEPTS } from '../data';
+import { Tile } from './Tile';
+
+export function Main() {
+  return (
+    <main>
+      <section id="core-concepts">
+        <h2>Core Concepts</h2>
+        <ul>
+          {CORE_CONCEPTS.map((concept) => (
+            <Tile title={concept.title} description={concept.description} image={concept.image} />
+            // <Tile {...concept} /> - does the same thing
+          ))}
+        </ul>
+      </section>
+    </main>
+  );
+}
