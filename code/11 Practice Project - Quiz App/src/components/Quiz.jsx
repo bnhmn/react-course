dbe0510828fbdc0cad6fc2f9c2280d20ad9438e8
Benjamin Heimann <benjamin.heimann@outlook.de>
2025-01-18 22:31:29 +0100

    Shuffle answers

--- code/11 Practice Project - Quiz App/src/components/Quiz.jsx
+++ code/11 Practice Project - Quiz App/src/components/Quiz.jsx
@@ -2,22 +2,45 @@ import { useState } from 'react';
 import { QUESTIONS } from '../data/questions';
 
 export function Quiz({ questions = QUESTIONS }) {
-  const [answers, setAnswers] = useState([]);
-  const activeQuestionIndex = answers.length;
+  const [selectedAnswers, setSelectedAnswers] = useState([]);
+  const activeQuestionIndex = selectedAnswers.length;
   const activeQuestion = questions[activeQuestionIndex];
+  const activeQuestionAnswers = shuffle(activeQuestion.answers);
 
-  const handleSelectAnswer = (answer) => setAnswers((answers) => [...answers, answer]);
+  const handleSelectAnswer = (answer) => setSelectedAnswers((answers) => [...answers, answer]);
 
   return (
     <div id="quiz">
       <h2>{activeQuestion.text}</h2>
       <ol id="answers">
-        {activeQuestion.answers.map((answer) => (
+        {activeQuestionAnswers.map((answer, index) => (
           <li key={answer} className="answer">
-            <button onClick={() => handleSelectAnswer(answer)}>{answer}</button>
+            <button onClick={() => handleSelectAnswer(answer)}>
+              <span className="letter">{getAnswerLetter(index)}</span>
+              <span className="text">{answer}</span>
+            </button>
           </li>
         ))}
       </ol>
     </div>
   );
+}
+
+/**
+ * Shuffle an array using Durstenfeld algorithm.
+ * @see https://stackoverflow.com/a/12646864/6316545.
+ */
+function shuffle(array) {
+  array = structuredClone(array);
+  for (let i = array.length - 1; i >= 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    const temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+  return array;
+}
+
+function getAnswerLetter(answerIndex, letters = ['A', 'B', 'C', 'D']) {
+  return letters[answerIndex];
 }
