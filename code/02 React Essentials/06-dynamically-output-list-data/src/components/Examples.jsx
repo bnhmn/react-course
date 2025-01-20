d11c6fb579f99701232c39063388b363aabc2c94
Benjamin Heimann <benjamin.heimann@outlook.de>
2025-01-20 22:21:47 +0100

    Add note about keys and state updates

--- code/02 React Essentials/06-dynamically-output-list-data/src/components/Examples.jsx
+++ code/02 React Essentials/06-dynamically-output-list-data/src/components/Examples.jsx
@@ -11,11 +11,19 @@ export function Examples() {
     <section id="examples">
       <h2>Examples</h2>
       <menu>
-        {Object.entries(EXAMPLES).map(([key, example]) => (
-          <TabButton key={key} isActive={selected === key} onClick={() => setSelected(key)}>
-            {example.title}
-          </TabButton>
-        ))}
+        {
+          // In JSX, you can output arrays like this: [<p>Hello</p>, <p>World!</p>].
+          // EXAMPLES is an array too. You can use map() to transform arrays into JSX elements.
+
+          Object.entries(EXAMPLES).map(([key, example]) => (
+            <TabButton key={key} isActive={selected === key} onClick={() => setSelected(key)}>
+              {example.title}
+            </TabButton>
+          ))
+
+          // When outputting arrays, each array item should have a unique key property.
+          // Without the key property, React will print a warning to the console.
+        }
       </menu>
       {/* There are other ways to do this. See https://react.dev/learn/conditional-rendering */}
       {!selected && <p>Please select a topic.</p>}
--- code/12 Behind the Scenes/03-reset-components-with-keys/src/components/CounterConfig.jsx
+++ code/12 Behind the Scenes/03-reset-components-with-keys/src/components/CounterConfig.jsx
@@ -8,6 +8,26 @@ export function CounterConfig({ onChange }) {
 
   function handleChange(event) {
     setEnteredNumber(event.target.value);
+
+    // Remember: State updates are not performed immediately, but are scheduled to run in a subsequent render cycle.
+    // Note: You cannot directly use the updated state value after setting it to a new value.
+    // This log statement will not log the new enteredNumber but the previous one!
+
+    console.log(enteredNumber);
+
+    // That's also the reason that you should use the setEnteredNumber(prev => prev + 1) variant if your new state
+    // depends on the old state value (not the case here though).
+    // This is especially import when your state setter function is executed multiple times in a render cycle.
+    //
+    // If you want to increment the enteredNumber by two, use this
+    //
+    // setEnteredNumber((prev) => prev + 1);
+    // setEnteredNumber((prev) => prev + 1);
+    //
+    // instead of this (will not work)
+    //
+    // setEnteredNumber(enteredNumber + 1);
+    // setEnteredNumber(enteredNumber + 1);
   }
 
   function handleSetClick() {
