a5e2e799c35711b8cbff2e2f51fbad22e1d129fe
Benjamin Heimann <benjamin.heimann@outlook.de>
2025-01-20 22:42:25 +0100

    Add 13 Class based Components

---
+++ code/13 Class based Components/src/components/User.js
@@ -0,0 +1,12 @@
+import { Component } from 'react';
+import classes from './User.module.css';
+
+export class User extends Component {
+  render() {
+    return <li className={classes.user}>{this.props.name}</li>;
+  }
+}
+
+// export function User(props) {
+//   return <li className={classes.user}>{props.name}</li>;
+// }
