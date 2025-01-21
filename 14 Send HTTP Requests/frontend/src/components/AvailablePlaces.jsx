bebbdf750609b978704cd61d57ca31d29e3a5e93
Benjamin Heimann <benjamin.heimann@outlook.de>
2025-01-21 19:23:37 +0100

    Show loading message

--- 14 Send HTTP Requests/frontend/src/components/AvailablePlaces.jsx
+++ 14 Send HTTP Requests/frontend/src/components/AvailablePlaces.jsx
@@ -4,10 +4,16 @@ import { Places } from './Places.jsx';
 
 export function AvailablePlaces({ onSelectPlace }) {
   const [places, setPlaces] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
   const backend = new BackendClient();
 
+  // Use an effect to fetch data from the backend. Other than that, it's very simple: just use fetch!.
+
   useEffect(() => {
-    backend.fetchAllPlaces().then(setPlaces);
+    backend
+      .fetchAllPlaces()
+      .then(setPlaces)
+      .finally(() => setIsLoading(false));
   }, []);
 
   return (
@@ -16,6 +22,7 @@ export function AvailablePlaces({ onSelectPlace }) {
       places={places}
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
+      isLoading={isLoading}
     />
   );
 }
