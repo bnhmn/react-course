a0ce549585e8f9770141915fc42b4b3eebc5ac49
Benjamin Heimann <benjamin.heimann@outlook.de>
2025-02-02 19:42:32 +0100

    Add description to homepage

--- 18 React Router/frontend/src/routes/HomePage.tsx
+++ 18 React Router/frontend/src/routes/HomePage.tsx
@@ -1,12 +1,20 @@
-import { Heading, Image, Text, useColorMode } from '@chakra-ui/react';
+import { Heading, Image, Link, Text, useColorMode } from '@chakra-ui/react';
 
 export function HomePage() {
   const { colorMode } = useColorMode();
   return (
     <>
-      <Heading mb="12">React Router Showcase</Heading>
-      <Image mb="12" maxW="25rem" src={`react-router-${colorMode}.svg`} alt="React logo" />
-      <Text fontSize="larger">Click a link in the top menu to explore the content!</Text>
+      <Heading mb="12">Homepage</Heading>
+      <Link href="https://reactrouter.com/home" target="_blank">
+        <Image mb="12" maxW="25rem" src={`react-router-${colorMode}.svg`} alt="React logo" />
+      </Link>
+      <Text mb="12" maxW="50rem" fontSize="larger">
+        Using react-router, you can simulate a multi-page application with React. <br />
+        You can create multiple routes between which the user can navigate back and forth. <br />
+        Click a link in the top menu to explore the content!
+      </Text>
+
+      <Text maxW="50rem" fontSize="larger"></Text>
     </>
   );
 }
