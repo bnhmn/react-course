905f8545af74df826847db3d0f2ca362becb123a
Benjamin Heimann <benjamin.heimann@outlook.de>
2025-02-02 12:02:14 +0100

    Add edit event button

--- 18 React Router/frontend/src/routes/events/ViewEventPage.tsx
+++ 18 React Router/frontend/src/routes/events/ViewEventPage.tsx
@@ -1,10 +1,11 @@
-import { useParams } from 'react-router';
+import { Link, useParams } from 'react-router';
 
-import { Heading, Spinner } from '@chakra-ui/react';
+import { EditIcon } from '@chakra-ui/icons';
+import { Heading, IconButton, Image, Spinner, Stack, Text } from '@chakra-ui/react';
 
 import { Breadcrumbs } from '../../components/Breadcrumbs';
-import { Event } from '../../components/Event';
 import { useEventBackend } from '../../lib/backend';
+import { formatDate } from '../../lib/localization';
 
 export function ViewEventPage() {
   const { eventId } = useParams();
@@ -14,7 +15,17 @@ export function ViewEventPage() {
       <Breadcrumbs />
       <Heading mb="10">Event Details</Heading>
       {isLoading && <Spinner />}
-      {!isLoading && event && <Event event={event} />}
+      {!isLoading && event && (
+        <Stack direction="column" gap="3">
+          <Stack direction="row" justifyContent="space-between" alignItems="center">
+            <Heading size="md">{event.title}</Heading>
+            <IconButton as={Link} to="edit" size="lg" icon={<EditIcon />} variant="ghost" aria-label="Edit event" />
+          </Stack>
+          <Text mb="3">{formatDate(event.date)}</Text>
+          <Image src={event.image} alt={event.title} />
+          <Text mt="3">{event.description}</Text>
+        </Stack>
+      )}
     </>
   );
 }
