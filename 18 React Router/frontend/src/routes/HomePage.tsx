27d572c6c52543db1144f0ecbcfd37f43560e285
Benjamin Heimann <benjamin.heimann@outlook.de>
2025-02-02 11:08:46 +0100

    Add better logo to HomePage

--- 18 React Router/frontend/src/routes/HomePage.tsx
+++ 18 React Router/frontend/src/routes/HomePage.tsx
@@ -1,13 +1,15 @@
-import { Heading, Image, Text } from '@chakra-ui/react';
+import { Heading, Image, Text, useColorMode } from '@chakra-ui/react';
+
+import { Breadcrumbs } from '../components/Breadcrumbs';
 
 export function HomePage() {
+  const { colorMode } = useColorMode();
   return (
     <>
-      <Heading mb="16">React Router Showcase</Heading>
-      <Image maxW="20rem" src="/react-router.svg" alt="React logo" mb="16" />
-      <Text fontSize="larger" mb="16">
-        Welcome! Click a link in the top menu to explore the content! 🚀
-      </Text>
+      <Breadcrumbs />
+      <Heading mb="12">React Router Showcase</Heading>
+      <Image mb="12" maxW="25rem" src={`react-router-${colorMode}.svg`} alt="React logo" />
+      <Text fontSize="larger">Click a link in the top menu to explore the content! 🚀</Text>
     </>
   );
 }
