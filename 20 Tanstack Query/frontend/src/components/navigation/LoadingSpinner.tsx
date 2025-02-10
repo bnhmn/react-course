3248abe5d01374155b95eac20204bbc8cebfa49b
Benjamin Heimann <benjamin.heimann@outlook.de>
2025-02-10 01:43:09 +0100

    Fix spinner position

--- 20 Tanstack Query/frontend/src/components/navigation/LoadingSpinner.tsx
+++ 20 Tanstack Query/frontend/src/components/navigation/LoadingSpinner.tsx
@@ -4,7 +4,15 @@ import { spinnerAccentColor } from '../../theme';
 
 export function LoadingSpinner() {
   return (
-    <Flex mt="20rem" align="center" justify="center" direction="column">
+    <Flex
+      position="fixed"
+      top="40%" // Adjusted to place the spinner a bit higher than the center
+      left="50%" // Centers horizontally on the page
+      transform="translate(-50%, -50%)" // Ensures proper centering of the spinner
+      direction="column"
+      align="center"
+      justify="center"
+    >
       <VStack spacing={6}>
         <Spinner boxSize="16" thickness="6px" speed="0.65s" color={spinnerAccentColor} />
         <Text fontSize="lg" fontWeight="medium">
